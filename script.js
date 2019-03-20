1f32ffcbba85286be3d82c8be45c87b3ecf643e8
Ashleyjeffers2002 <43543974+Ashleyjeffers2002@users.noreply.github.com>
2019-03-20 10:10:13 -0400

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,16 @@
+$(".White").click (function(){
+   $(".Power").toggle();
+    $("body").css("background-color","cadetBlue");
+});  
+
+$(".Pink").dblclick(function() {
+       $(".Pink").text("Find My Gold");
+  });
+
+
+$(".button2").click (function(){
+   $(".Gold").fadeToggle();
+});
+$(".Blue").click (function(){
+    $(".Weak").slideToggle();
+});
